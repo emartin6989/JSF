e9e9a37db2810337858446e3c4a44c1079a3f73a
coolsmos <martin@macbook-pro-de-martin.home>
2017-04-23 22:16:01 +0200

    TP Open Classrooms

---
+++ pro_jsf/src/com/sdzee/entities/Utilisateur.java
@@ -0,0 +1,64 @@
+package com.sdzee.entities;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Utilisateur {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	@Column(name = "mot_de_passe")
+	private String motDePasse;
+	private String nom;
+	@Column(name = "date_inscription")
+	private Timestamp dateInscription;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Timestamp getDateInscription() {
+		return dateInscription;
+	}
+
+	public void setDateInscription(Timestamp dateInscription) {
+		this.dateInscription = dateInscription;
+	}
+
+}
